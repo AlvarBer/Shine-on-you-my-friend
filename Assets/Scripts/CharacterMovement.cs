e324b2669ab682b06e2f288501828af0e3319663
unknown <bescobar@twitter.com>
2018-10-14 00:29:12 +0200

    Add enemy and player movement

---
+++ Assets/Scripts/CharacterMovement.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent (typeof (Rigidbody2D))] 
+public class CharacterMovement : MonoBehaviour {
+
+	public float speed = 4f;
+	public Vector2 direction;
+
+	protected void Move() {
+		GetComponent<Rigidbody2D>().velocity = direction * speed;
+		// print("BL: Object(" + this.name + ") has vect " + direction);
+	}
+}
---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : CharacterMovement {
+
+	void FixedUpdate () {
+		this.direction = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"));
+		this.Move();
+	}
+}
