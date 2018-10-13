d7c0221b4be29c63862708818afc407931c694e7
unknown <bescobar@twitter.com>
2018-10-14 01:03:01 +0200

    Very crude boxes

--- Assets/Scripts/CharacterMovement.cs
+++ Assets/Scripts/CharacterMovement.cs
@@ -6,10 +6,4 @@ using UnityEngine;
 public class CharacterMovement : MonoBehaviour {
 
 	public float speed = 4f;
-	public Vector2 direction;
-
-	protected void Move() {
-		GetComponent<Rigidbody2D>().velocity = direction * speed;
-		// print("BL: Object(" + this.name + ") has vect " + direction);
-	}
 }
--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -4,8 +4,16 @@ using UnityEngine;
 
 public class PlayerMovement : CharacterMovement {
 
+	public Vector2 direction;
+	protected void Move() {
+		var posDelta = (direction * speed);
+		transform.SetPositionAndRotation(
+			transform.position + new Vector3(posDelta.x, posDelta.y, 0),
+			transform.rotation
+		);
+	}
 	void FixedUpdate () {
 		this.direction = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"));
-		this.Move();
+		Move();
 	}
 }
