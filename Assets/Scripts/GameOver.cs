b7665baf9236274495dc3cefba2995d3fbb3e1b7
unknown <bescobar@twitter.com>
2018-10-14 17:21:15 +0200

    Yata!

---
+++ Assets/Scripts/GameOver.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameOver : MonoBehaviour {
+	private void Update() {
+		if (Input.GetButtonDown("Submit")) {
+			EventsManager.Instance.RouteEvent(this, new NextLevelEvent(EventsManager.EventType.NEXT_LEVEL, "Menu"));
+		}
+	}
+}
