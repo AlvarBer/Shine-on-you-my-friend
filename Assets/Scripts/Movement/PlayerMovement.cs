79ad712591327c9208186ee231ae3ac596df046a
unknown <bescobar@twitter.com>
2018-10-14 14:58:05 +0200

    Add some other things

--- Assets/Scripts/Movement/PlayerMovement.cs
+++ Assets/Scripts/Movement/PlayerMovement.cs
@@ -5,15 +5,39 @@ using UnityEngine;
 public class PlayerMovement : CharacterMovement {
 
 	public Vector2 direction;
-	protected void Move() {
+
+    private Animator animator;
+
+    private void Awake()
+    {
+        animator = GetComponent<Animator>();
+    }
+
+    protected void Move() {
 		var posDelta = (direction * speed);
 		transform.SetPositionAndRotation(
 			transform.position + new Vector3(posDelta.x, posDelta.y, 0),
 			transform.rotation
 		);
 	}
+
+    protected void Animate()
+    {
+        if (direction.x >= 0)
+        {
+            transform.localScale = new Vector3(1f, 1f, 1f);
+        }
+        else
+        {
+            transform.localScale = new Vector3(-1f, 1f, 1f);
+        }
+        animator.SetFloat("DirectionX", direction.x);
+        animator.SetFloat("DirectionY", direction.y);
+    }
+
 	void FixedUpdate () {
 		this.direction = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"));
 		Move();
+        Animate();
 	}
 }
