a62fec78c682a4bf694c3d44c8781de44de2ce23
unknown <bescobar@twitter.com>
2018-10-14 15:36:04 +0200

    Add victory and start screens

---
+++ Assets/Scripts/StartGame.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StartGame : MonoBehaviour {
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetButton("Submit")) {
+			EventsManager.Instance.RouteEvent(this, new NextLevelEvent(EventsManager.EventType.NEXT_LEVEL, "Level 1"));
+		}
+	}
+}
