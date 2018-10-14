fa4cca13d91c17ec9e6d1842fa6f25be7d731c06
unknown <bescobar@twitter.com>
2018-10-14 10:43:40 +0200

    Add light toggles

---
+++ Assets/Scripts/Ligths/LightSourceActivators.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LightSourceActivators : MonoBehaviour {
+
+	// Because negative int modulo sucks
+	int mod(int x, int m) {
+		return (x%m + m)%m;
+	}
+
+	public LightSource[] lightSources;
+	private int lightSourceIndex;
+
+	void Start () {
+		foreach (var light in lightSources) {
+			light.Off();
+		}
+		lightSources[lightSourceIndex].On();
+	}
+	
+	void moveIndex(int delta) {
+		lightSources[lightSourceIndex].Off();
+		print("BL: index " + lightSourceIndex + " delta " +  delta + " result " +  mod((lightSourceIndex + delta), lightSources.Length));
+		lightSourceIndex = mod((lightSourceIndex + delta), lightSources.Length);
+		lightSources[lightSourceIndex].On();
+	}
+
+	void Update () {
+		var f = Input.GetButtonDown("Switch Lights Forwards");
+		var b = Input.GetButtonDown("Switch Lights Backwards");
+		if (f && !b) {
+			moveIndex(1);
+		} 
+		if (b && !f) {
+			moveIndex(-1);
+		}
+	}
+}
