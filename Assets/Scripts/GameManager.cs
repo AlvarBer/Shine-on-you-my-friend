995580f495dd3ea1d13832925b93aa90c791fab4
Laura <laura.dcs4@gmail.com>
2018-10-14 01:51:06 +0100

    Added game over scene and game manager

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameManager : Singleton<GameManager>
+{
+    private void Awake()
+    {
+        EventsManager.Instance.SubscribeTo(EventsManager.EventType.TARGET_DEATH, OnPlayerDeath);
+    }
+
+    private void OnPlayerDeath(object sender, BaseEvent e)
+    {
+        SceneManager.LoadScene("GameOver");
+    }
+}
