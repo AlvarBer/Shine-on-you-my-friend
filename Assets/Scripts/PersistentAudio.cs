394591549debe136fe7f59374dbb2e2eb7e197af
unknown <bescobar@twitter.com>
2018-10-14 16:34:19 +0200

    Add music

---
+++ Assets/Scripts/PersistentAudio.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class TrueSingleton<T> : Singleton<T> where T : MonoBehaviour {
+    protected override void Awake() {
+        base.Awake();
+        DontDestroyOnLoad(this.gameObject);
+    }
+}
+public class PersistentAudio : TrueSingleton<PersistentAudio> {
+
+}
